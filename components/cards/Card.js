55f186c0fcc383c236057e0e571f2d81c7fd917e
Imam <imam.tauhid.dar@icloud.com>
2018-12-25 15:27:08 +0700

    update

--- components/cards/Card.js
+++ components/cards/Card.js
@@ -6,7 +6,7 @@ function downloadImage (image) {
 		method: 'GET',
 		responseType: 'blob', // important
 	}).then((response) => {
-		const url = window.URL.createObjectURL(new Blob([response.data]));
+		const url = window.URL.createObjectURL(new Blob([response.data], {type: 'image/jpeg'}));
 		const link = document.createElement('a');
 		link.href = url;
 		link.setAttribute('download', 'cert.jpeg');
